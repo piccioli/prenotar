6812aa66bd0040f300a6a03fac742b9b7fcee533
Alessio Piccioli <piccioli@netseven.it>
2026-07-15 07:46:33 +0200

    chore: rimuove il widget placeholder "Pannello Sezione in costruzione"
    App\Filament\Sezione\Widgets\PlaceholderWidget era uno scaffold di una
    fase iniziale di sviluppo, mai rimosso: veniva auto-registrato da
    discoverWidgets() insieme al vero PrenotazioniDashboardWidget e mostrava
    un messaggio fuorviante ("le funzionalità... saranno disponibili nella
    Fase 3 del progetto") sopra il contenuto reale della dashboard Sezione,
    nonostante il wizard di prenotazione sia completo da tempo. Trovato
    durante la verifica visiva post-deploy su develop.
    
    Rimossi la classe e la relativa view; nessun test la referenziava.

--- resources/views/filament/sezione/widgets/placeholder.blade.php
+++
@@ -1,9 +0,0 @@
-<x-filament-widgets::widget>
-    <x-filament::section>
-        <x-slot name="heading">Pannello Sezione in costruzione</x-slot>
-
-        <p class="text-sm text-gray-600 dark:text-gray-400">
-            Le funzionalità di prenotazione delle torri di arrampicata saranno disponibili nella Fase 3 del progetto.
-        </p>
-    </x-filament::section>
-</x-filament-widgets::widget>
